b6632bbb4b3d7a1f1fc99b61f9623c870035e159
DenysMaiko <maikodenys@gmail.com>
2025-05-05 20:15:21 +0300

    Bare-bone system was created

---
+++ application/distance-learning-app/frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Система дистанційного навчання</h1>
+      <p>Вітаємо у вашій системі дистанційного навчання!</p>
+      {/* Тут згодом будуть компоненти для користувачів, дисциплін, оцінок */}
+    </div>
+  );
+}
+
+export default App;
